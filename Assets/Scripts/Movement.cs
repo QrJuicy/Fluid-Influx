168d5c38dfc1b41e058b5dd6d16152233cb259ce
Ben-Jacks0n <1.moment.i.got.hacked@gmail.com>
2023-03-18 23:45:13 +0700

    Updared movement ( not finished)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,6 +10,8 @@ public class Movement : MonoBehaviour
     [SerializeField] float movSpeed = 5f;
     [SerializeField] float jumpForce = 5f;
 
+    private float horizontalInput;
+    [SerializeField] Transform orientation;
 
     
     
@@ -25,7 +27,7 @@ public class Movement : MonoBehaviour
        
         
         //Moving the x axis position
-        transform.Translate(Vector3.right * Time.deltaTime * movSpeed * Input.GetAxis("Horizontal"));
+        movingXaxis();
 
         //Jumping
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) && isOnGround == true)
@@ -34,5 +36,27 @@ public class Movement : MonoBehaviour
             isOnGround = false;
         }
     }
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
 
+    private void movingXaxis()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    private void MovePlayer()
+    {
+        myRigidbody.AddForce(orientation.right.normalized * horizontalInput * movSpeed * 10f, ForceMode.Force);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+        isOnGround = true; 
+        Debug.Log("istouching ground");
+        }   
+    }
 }
