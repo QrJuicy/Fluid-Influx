b1289d96b05d6e75f3c0e0713fa04ebe2ea5bea0
Ben-Jacks0n <1.moment.i.got.hacked@gmail.com>
2023-03-20 15:02:25 +0700

    Step up stairs (not finished)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -18,10 +18,18 @@ public class Movement : MonoBehaviour
     [SerializeField] float playerHeight;
     [SerializeField] LayerMask Ground;
     [SerializeField] float groundDrag;
+    [SerializeField] bool boolLower;
+    [SerializeField] bool boolUpper;
     private bool grounded;
 
     [Header("fucking gravity (set to 0 if default)")]
     [SerializeField] float customGravity;
+
+    [Header("Player Step Up Stairs")]
+    [SerializeField] GameObject stepRayUpper;
+    [SerializeField] GameObject stepRayLower;
+    [SerializeField] float stepHeight = 0.3f;
+    [SerializeField] float stepSmooth = 2f;
 
     
     
@@ -30,7 +38,6 @@ public class Movement : MonoBehaviour
     {
         myRigidbody = gameObject.GetComponent<Rigidbody>();
     }
-
     
     void Update()
     {
@@ -38,14 +45,15 @@ public class Movement : MonoBehaviour
         dragWhenMoving();
         movingXaxis();
         airMoveLimit();
+        stepClimb();
     }
 
     void FixedUpdate()
     {
         myRigidbody.AddForce(new Vector3(0, -1.0f, 0)*myRigidbody.mass*customGravity);
         MovePlayer();
-        
         jumpUp();
+        
     }
 
     //Jumping
@@ -65,7 +73,7 @@ public class Movement : MonoBehaviour
         if(!grounded && (myRigidbody.velocity.x > movSpeed|| myRigidbody.velocity.x < -movSpeed ))
         {
 
-                myRigidbody.velocity = new Vector3(myRigidbody.velocity.x * 0.8f , myRigidbody.velocity.y , myRigidbody.velocity.z);
+            myRigidbody.velocity = new Vector3(myRigidbody.velocity.x * 0.8f , myRigidbody.velocity.y , myRigidbody.velocity.z);
 
         }
     }
@@ -74,6 +82,10 @@ public class Movement : MonoBehaviour
     private void groundChecking()
     {
         grounded = Physics.Raycast(transform.position, Vector3.down,playerHeight * 0.5f + 0.2f , Ground);
+        RaycastHit hitLower;
+        boolLower = Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.left), out hitLower, 0.1f);
+        RaycastHit hitUpper;
+        boolUpper = Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.left), out hitUpper, 0.2f);
     }
 
     private void dragWhenMoving()
@@ -97,5 +109,18 @@ public class Movement : MonoBehaviour
     {
         myRigidbody.AddForce(orientation.right.normalized * horizontalInput * movSpeed  *  10f , ForceMode.Force);
     }
+    
+        void stepClimb()
+    {
+        if (boolLower == true)
+        {
+            Debug.Log("hitting lower");
+            if (boolUpper == false)
+            {
+                myRigidbody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+                Debug.Log("hittingUpper");
+            }
+        }
 
+    }
 }
